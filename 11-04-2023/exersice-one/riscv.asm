629904d147e67e607328fac4621af0105766937a
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-04-12 20:59:16 -0300

    fix: fixing sum logic

--- 11-04-2023/exersice-one/riscv.asm
+++ 11-04-2023/exersice-one/riscv.asm
@@ -1,26 +1,31 @@
 .data
+	# Constants data
 	firstQuestion: .asciz "Enter an integer number: "
 	secondQuestion: .asciz "Enter other integer number: "
 	result: .asciz "This is result of sum: "
 
 .text
-	addi a7, zero, 4
-	la a0, firstQuestion
-	ecall
-	addi a7, zero, 5
-	ecall
+	addi a7, zero, 4 					# Register a7 must to show string
+	la a0, firstQuestion 			# Load first question message
+	ecall 											# Call register a7 to show first question message
+	addi a7, zero, 5 					# Register a7 must to receive int 
+	ecall											# Ask an int value
 	
 	add s0, zero, a0
 	
-	addi a7, zero, 4
-	la a0, secondQuestion
-	ecall
-	addi a7, zero, 5
-	ecall
+	addi a7, zero, 4						# Register a7 must to show string
+	la a0, secondQuestion			# Load second question message
+	ecall											# Call register a7 to show second question message
+	addi a7, zero, 5						# Register a7 must to receive int 
+	ecall											# Ask an int value
 	
-	# This code has error in yours logic
 	add t1, s0, a0	
-	add a7, zero, t1
-	li a7, 1
-	ecall
+	
+	addi a7, zero, 4						# Register a7 must to show string
+	la a0, result							# Load result message
+	ecall											# Call register a7 to show result message
+	
+	add a0, zero, t1	
+	li a7, 1										# Load a0 value (li a7, 1 = a0 in console) 
+	ecall											# Show int value
 	
